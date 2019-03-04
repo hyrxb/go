bf359e658b8e6c1dbed052162c1bbe2b87e3740d
hyrxb <hyrxb@163.com>
2019-03-04 02:34:26 -0500

    浮点类型

--- src/main/浮点类型.go
+++ src/main/浮点类型.go
@@ -1,1 +1,16 @@
 package main
+
+import "fmt"
+
+func main() {
+
+	var f1 float32
+	fmt.Println("f1 = ", f1)
+
+	//自动推导类型
+
+	f2 := 3.14
+	fmt.Println("f2 = ", f2)
+	fmt.Printf("f2 type is %T", f2)
+
+}
